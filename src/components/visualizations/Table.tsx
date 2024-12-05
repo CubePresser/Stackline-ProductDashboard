76451904863cfa4782fc0605f485a031b1b52624
J. Alex Jones <cubepresser@gmail.com>
2024-12-04 22:10:41 -0800

    Add keys

--- src/components/visualizations/Table.tsx
+++ src/components/visualizations/Table.tsx
@@ -21,38 +21,40 @@ export const Table: React.FC<TableProps> = ({
 }) => {
   return (
     <table>
-      <tr>
+      <tbody>
+        <tr>
+          {
+            data.headers.map(headers => <th key={headers.id}>{headers.title}</th>)
+          }
+        </tr>
         {
-          data.headers.map(headers => <th key={headers.id}>{headers.title}</th>)
+          data.rows.map((row, rowIdx) => (
+            <tr key={`row-${rowIdx}`}>
+              {
+                row.map((cell, idx) => (
+                  <td key={`cell-${idx}`}>
+                    {
+                      (() => {
+                        switch(data.headers[idx].type) {
+                          case 'date':
+                            return Intl.DateTimeFormat('en-US', { dateStyle: 'short' }).format(new Date((cell as string).replace(/-/g, '/'))).replace(/\//g, '-',);
+                          case 'currency':
+                            return Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', maximumFractionDigits: 0 }).format(cell as number);
+                          case 'number':
+                            return Intl.NumberFormat('en-US').format(cell as number);
+                          case 'string':
+                          default:
+                            return cell;
+                        }
+                      })()
+                    }
+                  </td>
+                ))
+              }
+            </tr>
+          ))
         }
-      </tr>
-      {
-        data.rows.map(row => (
-          <tr>
-            {
-              row.map((cell, idx) => (
-                <td>
-                  {
-                    (() => {
-                      switch(data.headers[idx].type) {
-                        case 'date':
-                          return Intl.DateTimeFormat('en-US', { dateStyle: 'short' }).format(new Date((cell as string).replace(/-/g, '/'))).replace(/\//g, '-',);
-                        case 'currency':
-                          return Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', maximumFractionDigits: 0 }).format(cell as number);
-                        case 'number':
-                          return Intl.NumberFormat('en-US').format(cell as number);
-                        case 'string':
-                        default:
-                          return cell;
-                      }
-                    })()
-                  }
-                </td>
-              ))
-            }
-          </tr>
-        ))
-      }
+      </tbody>
     </table>
   )
 };
