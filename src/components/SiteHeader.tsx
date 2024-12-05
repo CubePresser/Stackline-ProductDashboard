df3f5cefd15ba0210062d8e630b59fadc45251b4
J. Alex Jones <cubepresser@gmail.com>
2024-12-04 17:04:26 -0800

    Update site header

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 export const SiteHeader: React.FC = () => {
   return (
-    <header id="site-header">Site Header</header>
+    <header id="site-header">
+      <img id="site-icon" src="https://cdn.prod.website-files.com/63eeb6755510a42d663dba4c/640687eb9b50f625f22f95e6_Favicon%20256px%20x%20256px.png" />
+    </header>
   )
 };
