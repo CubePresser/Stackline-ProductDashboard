4a2f452273d85b3a91224e341addf373311c75a2
J. Alex Jones <cubepresser@gmail.com>
2024-12-04 18:04:53 -0800

    ProductOverview

--- src/components/ProductOverview.tsx
+++ src/components/ProductOverview.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Product } from '../types';
 
 type ProductOverviewProps = {
@@ -8,12 +8,26 @@ type ProductOverviewProps = {
 export const ProductOverview: React.FC<ProductOverviewProps> = ({
   product
 }) => {
+  const [ loadingImg, setLoadingImg ] = useState(true);
+
   return (
     <article className="product-overview">
       {product ? (
         <>
+        <img
+          src={product.image}
+          onLoad={() => setLoadingImg(true)}
+          style={ loadingImg ? {} : { display: 'none' } }
+        />
         <h1>{ product.title }</h1>
         <p>{ product.subtitle }</p>
+        <ul className="tags-list">
+          {
+            product.tags.map(tag => (
+              <li className="tag">{tag}</li>
+            ))
+          }
+        </ul>
         </>
       ): null}
     </article>
